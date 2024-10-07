3088f79e08f2ae78e51e089a863c2b31858e5ffa
Francesco Piscani <fpiscani@gmail.com>
2024-10-06 20:30:02 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Alert from "./components/Alert";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Alert text="Hello, world!" />
+    </div>
+  );
+};
+
+export default App;
